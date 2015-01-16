3e6aaa1094c51379265917a34a585845e22cffcd
wang.xin <wang.xin@epc-china.com>
2015-01-16 16:49:36 +0800

    support hover event.

--- js/jquery.imgmarker.js
+++ js/jquery.imgmarker.js
@@ -1,16 +1,5 @@
 /*
- pos:
- [
- {
- "x":2,
- "y":200,
- "w":120,
- "h":50,
- "point":"center-middle",
- "title":"华润支路",
- "event":"event01"
- }
- ]
+
  */
 ;(function($){
 	$.fn.imgmarker=function(opts){
@@ -24,7 +13,8 @@
 		
 		$.extend(settings,opts);
 		return this.each(function(){
-			var $this=$(this),$img=$("<div />");
+			var $this=$(this),$img=$("<div />"),_z=1000;
+			$this.empty();
 			$img.css({"background-image":"url('"+settings.imgurl+"')",
 					  "background-repeat":"no-repeat",
 					  "position":"relative"
@@ -33,6 +23,7 @@
 			$this.html($img);
 			$.each(settings.pos,function(){
 				var $div=$("<div />"),p=this,_x=0,_y=0,po1,po2;
+				_z++;
 				if(p.point&&p.point.indexOf("-")>0){
 					po1=p.point.split("-")[0];
 					po2=p.point.split("-")[1];
@@ -51,12 +42,44 @@
 						_y=p.y;
 					}
 				}
-				$div.css({"border":"none","position":"absolute","left":_x+"px","top":_y+"px","cursor":"pointer"});
-				$div.width(p.w).height(p.h).attr("title",p.title);
+				$div.css({"border":"none","position":"absolute","left":_x+"px","top":_y+"px","cursor":"pointer","z-index":_z});
+				$div.width(p.w).height(p.h);
+				if(p.title&&$.trim(p.title).length>0)$div.attr("title",p.title);
 				$img.append($div);
-				if(typeof(settings.events[p.event])=="function"){
-					$div.click(function(event){settings.events[p.event].call(p,event);});
+				switch(typeof(p.event)){
+				case "string":
+					if(typeof(settings.events[p.event])=="function"){
+						$div.click(function(p,settings,$img){return function(event){
+							settings.events[p.event].call(p,event,{"point":$(this),"img":$img});
+						};}(p,settings,$img));
+					}
+					break;
+				case "object":
+					for(var key in p.event){
+						var _key=key;
+						if(_key.toLowerCase()=="hover"){
+							if(typeof(p.event[_key])=="string"){
+								$div.hover(function(p,settings,_key,$img){return function(event){
+									settings.events[p.event[_key]].call(p,event,{"point":$(this),"img":$img});
+								};}(p,settings,_key,$img),function(p,settings,_key,$img){return function(event){
+									settings.events[p.event[_key]].call(p,event,{"point":$(this),"img":$img});
+								};}(p,settings,_key,$img));
+							}else if(typeof(p.event[_key])=="object"){
+								$div.hover(function(p,settings,_key,$img){return function(event){
+									settings.events[p.event[_key][0]].call(p,event,{"point":$(this),"img":$img});
+								};}(p,settings,_key,$img),function(p,settings,_key,$img){return function(event){
+									settings.events[p.event[_key][1]].call(p,event,{"point":$(this),"img":$img});
+								};}(p,settings,_key,$img));
+							}
+						}else{
+							$div.bind(_key,function(p,settings,_key,$img){return function(event){
+								settings.events[p.event[_key]].call(p,event,{"point":$(this),"img":$img});
+							};}(p,settings,_key,$img));
+						}
+					}
+					break;
 				}
+				
 			});
 		});
 	};
